6ac5575ed47c5a537f73a2951285b5cb1aaea826
suhas <suhas.manjunath@inmar.com>
2019-07-24 12:15:48 +0530

    Setting up webotron project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # automating-aws-with-python
-Boto3, Rekognition,AWS
+Boto3, Rekognition, Python
+
+
+## 01-webotron
+
+Webotron project is a python script which will sync local directory to s3 bucket. Also, you would learn to configure Route53 and Cloudfront
+
--- boto3examples/boto3-s3-create-bucket.py
+++ boto3examples/boto3-s3-create-bucket.py
@@ -10,6 +10,6 @@ s3 = session.resource('s3')
 # create bucket
 # aws s3 mb s3://suhas-search-engine --profile pythonAutomation
 
-# need location constraint
+# need location constraint for regions othe than us-east-1
 new_bucket = s3.create_bucket(Bucket='automating-aws-boto3-' + str(
     fake.random_int()), CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
